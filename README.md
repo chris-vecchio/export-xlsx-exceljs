60aa2cd420196c4a2ac6304b1e49f2165d5d09cc
ChrisV <chris.vecchio.econ@gmail.com>
2021-01-05 16:15:35 -0500

    Version 1.06 Update

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
-﻿
-# Export XLSX - Highcharts module
+﻿Export XLSX - Highcharts module
+===============================
 
-This plugin adds the capability to export from Highcharts to the Excel .xlsx format. This is an updated version of the original [export-xlsx](https://www.highcharts.com/plugin-registry/single/57/Export-xlsx) plugin. I removed the moment.js and jQuery requirements and switched to using the [exceljs](https://github.com/exceljs/exceljs) library for exporting. Using exceljs makes available more options for custom export formatting.
+This plugin adds the capability to export from Highcharts to the Excel .xlsx format. This is an updated version of the original [export-xlsx](https://www.highcharts.com/plugin-registry/single/57/Export-xlsx) plugin. I removed the moment.js and jQuery requirements and switched to using the [ExcelJS](https://github.com/exceljs/exceljs) library for exporting. ExcelJS provides many more options for custom export formatting than SheetJS.
 
 ### Demo
 
-* https://jsfiddle.net/chrisvecchio/4p0b1muf/
+* https://jsfiddle.net/chrisvecchio/ds6ocjy0/
 
 ### Requirements
 
-* Latest Highcharts (tested with 8.0.0), but should work with version 6.1.0+.
+* Latest Highcharts (tested with 8.0.0), but should work with version 7.0.0+.
 * Latest Highcharts [exporting](https://code.highcharts.com/modules/exporting.js) and [export-data](https://code.highcharts.com/modules/export-data.js) modules.
-* [exceljs](https://github.com/exceljs/exceljs) version 3.5.0+.
+* [ExcelJS](https://github.com/exceljs/exceljs) version 3.5.0+.
 * [FileSaver.js](https://github.com/eligrey/FileSaver.js) version 2.0.2+.
 * [SSF.js](https://github.com/SheetJS/ssf/) version 0.10.2+. (only needed if using auto column width feature)
 
 **Notes:**
 
-- Plugin does NO verification of exceljs options specifications. I recommend [google](https://www.google.com/), [stackoverflow](https://stackoverflow.com/questions/tagged/exceljs) or the official exceljs [docs](https://github.com/exceljs/exceljs/) for exceljs questions.
+- Plugin does NO verification of ExcelJS options specifications. I recommend [google](https://www.google.com/), [stackoverflow](https://stackoverflow.com/questions/tagged/exceljs) or the official ExcelJS [docs](https://github.com/exceljs/exceljs/) for ExcelJS questions.
 - Plugin does not check for correctly specified Excel number/date formats. A description of Excel's number format codes by Microsoft can be found [here](https://support.office.com/en-us/article/number-format-codes-5026bbd6-04bc-48cd-bf33-80f18b4eae68) . There are plenty of other resources available on Excel number formats via a google search or stackoverflow.
 
 ### Installation
@@ -41,19 +41,16 @@ Excel worksheet name (Excel restricts sheet name length to <= 31 characters) Def
 Category column title in Excel. Default: `Highcharts default`
 ##### `categoryColumn.numberFormat` (String)
 Category column Excel number format. Default: `undefined` for non-datetime x-axis, `yyyy-mm-dd hh:mm:ss` for datetime x-axis.
-##### `headerStyle.font.color` (String)
-Column header font hexadecimal color. Default: `undefined`
-##### `headerStyle.font.bold` (Boolean)
-Make column header font bold. Default: `undefined`
-##### `headerStyle.fill.color` (String)
-Column header fill hexadecimal color. Default: `undefined`
+##### Experimental options
+##### `headerStyle` (Object)
+Column header row font and fill options. See [example options](#example-options) for supported attributes Default: `{}`
 
 
 #### workbook
 ##### `fileProperties ` (Object)
-Excel file properties. List of [available properties](https://github.com/exceljs/exceljs/blob/master/lib/doc/workbook.js#L153). Default: `null`
+Excel workbook file properties. List of [available properties](https://github.com/exceljs/exceljs/blob/master/lib/doc/workbook.js#L153). Default: `{}`
 
-
+#### series
 _The plugin adds the following new options to individual_ `series` _options under the_ `xlsx` _key_.
 ##### `name` (String)
 Column title for series in exported Excel file Default: `series name`
@@ -101,3 +98,17 @@ series: [{
     data: [6.9, 7.3, 6.9, 7.1, 6.6, 6.7, 7.3, 6.5, 7.6, 7.3, 7.7, 7.7, 7.7, 7.4, 8.4, 7.7, 8.1, 8.7, 8.6, 9.7, 9.8, 10.3, 10.8, 11.1, 12.4, 13.2, 14.0, 14.9, 15.2, 15.6, 15.3, 15.6, 14.9, 15.2, 14.7, 15.0, 15.3, 15.8, 14.9, 14.7, 14.6, 14.2, 13.5, 14.1, 15.6, 15.0, 15.4, 15.0, 14.3, 14.0, 14.1, 14.7, 14.5, 14.4, 14.5, 14.1, 14.3, 13.5, 12.8, 13.7, 13.0, 13.1, 12.8, 12.5, 12.9, 12.6, 12.4, 11.8, 11.7, 12.1, 12.0, 11.8, 12.0, 11.3, 11.1, 11.6, 11.0, 10.7, 10.8, 11.1, 10.5, 10.9, 10.7, 9.8, 9.4, 9.8, 9.4, 8.7, 9.2, 9.2, 9.5, 9.2, 8.5, 8.1, 8.6, 8.6, 8.3, 8.2, 8.6, 8.5, 8.7, 8.2, 8.3, 7.9, 7.9, 7.6, 6.8, 6.5, 7.1, 7.0, 7.4, 7.6, 7.5, 7.6, 6.4, 7.4, 8.5, 7.5, 7.8, 7.6, 7.4, 7.6, 6.6, 6.4, 6.3, 6.5, 7.0, 6.1, 6.7, 6.0, 5.2, 6.3, 5.5, 5.6, 5.6, 5.8, 5.5, 5.6, 5.0, 5.7, 5.6, 5.9, 5.6, 5.8, 5.7]
 }]
 ```
+
+# Changelog
+
+## [1.06] - 2021-01-05
+
+### Added
+- Now compatible with a custom [columnHeaderFormatter](https://api.highcharts.com/highcharts/exporting.csv.columnHeaderFormatter) function
+- Correctly sets column headers for [HighCharts combination chart](https://www.highcharts.com/demo/combo)
+- Hours, minutes, and seconds date parts are now correctly exported and formatted. Previously, only month, year, and day were exported for a dateTime xAxis. 
+### Changed
+- Minimum supported Highcharts version updated bumped to 7.0.0 from 6.1.0
+- Rows are no longer filtered to visible data for Pie charts [Issue #2](https://github.com/chris-vecchio/export-xlsx-exceljs/issues/2)
+- Date/time values are recognized as Excel dates and formatted correctly in the exported file
+- Updated docs and added several examples to index.html
--- js/export-xlsx.js
+++ js/export-xlsx.js
@@ -2,19 +2,6 @@
     // Returns the first value that is not null or undefined.
     var pick = H.pick;
 
-    // This extends the getDataRows function to only include rows for points visible in
-    // the current chart view. Author: Torstein Honsi
-    // Source: https://github.com/highcharts/highcharts/issues/7913#issuecomment-371052869
-    H.wrap(H.Chart.prototype, 'getDataRows', function(proceed, multiLevelHeaders) {
-        var rows = proceed.call(this, multiLevelHeaders),
-            xMin = this.xAxis[0].min,
-            xMax = this.xAxis[0].max;
-        rows = rows.filter(function(row) {
-            return typeof row.x !== 'number' || (row.x >= xMin && row.x <= xMax);
-        });
-        return rows;
-    });
-
     // Add "Download XLSX" to the exporting menu in place of "Download XLS". Source:
     // https://jsfiddle.net/gh/get/library/pure/highcharts/highcharts/tree/master/samples/highcharts/export-data/xlsx/
     H.getOptions().lang.downloadXLSX = 'Download XLSX';
@@ -31,22 +18,56 @@
     var menuItems = H.getOptions().exporting.buttons.contextButton.menuItems;
     menuItems[menuItems.indexOf('downloadXLS')] = 'downloadXLSX';
 
+    // Moved initalization of xlsx export options to addEvent so they are defined on
+    // chart load and hence can be referenced in the chart load function
+    H.addEvent(H.Chart, 'load', function(e) {
+        var chart = e.target;
+        // Check if all series are pie series. Need to disable filtered getDataRows function
+        // for pie series or only the first row is exported.
+        // See https://github.com/chris-vecchio/export-xlsx-exceljs/issues/2
+        var allPieSeries = false;
 
-    // Add default XLSX exporting options to Chart init function
-    H.wrap(H.Chart.prototype, 'init', function(proceed) {
-        var exporting = arguments[1].exporting;
+        var seriesTypes = [];
+        chart.series.forEach(function(series, index) {
+            seriesTypes.push(series.type);
+        });
+
+        // Remove duplicates and convert back to array
+        seriesTypes = Array.from(new Set(seriesTypes));
+
+        // If there is only one series type and it is 'pie' series type
+        if (seriesTypes.length == 1 && seriesTypes[0] == 'pie') allPieSeries = true;
+
+        var exporting = chart.options.exporting;
         if (exporting) {
             exporting.xlsx = exporting.xlsx || {};
+            // worksheet options
             exporting.xlsx.worksheet = exporting.xlsx.worksheet || {};
             exporting.xlsx.worksheet.autoFitColumns = exporting.xlsx.worksheet.autoFitColumns || false;
             exporting.xlsx.worksheet.sheetName = exporting.xlsx.worksheet.sheetName || 'Sheet1';
             exporting.xlsx.worksheet.categoryColumn = exporting.xlsx.worksheet.categoryColumn || {};
             exporting.xlsx.worksheet.headerStyle = exporting.xlsx.worksheet.headerStyle || {};
+            // workbook options
             exporting.xlsx.workbook = exporting.xlsx.workbook || {};
             exporting.xlsx.workbook.fileProperties = exporting.xlsx.workbook.fileProperties || {};
             exporting.xlsx = deepClone(exporting.xlsx);
+            // check for all pie series
+            chart.options.exporting.xlsx.allPieSeries = allPieSeries;
         }
-        proceed.apply(this, Array.prototype.slice.call(arguments, 1));
+    });
+
+    H.wrap(H.Chart.prototype, 'getDataRows', function(proceed, multiLevelHeaders) {
+        var rows = proceed.call(this, multiLevelHeaders),
+            xMin = this.xAxis[0].min,
+            xMax = this.xAxis[0].max;
+        // Only filter rows if all series are not 'pie' series
+        // See https://github.com/chris-vecchio/export-xlsx-exceljs/issues/2
+        if (this.options.exporting.xlsx.allPieSeries == false) {
+            rows = rows.filter(function(row) {
+                return typeof row.x !== 'number' || (row.x >= xMin && row.x <= xMax);
+            });
+        }
+        return rows;
     });
 
     // Add default XLSX exporting options to Series init function
@@ -87,17 +108,58 @@
       return !obj || Object.keys(obj).length === 0;
     }
 
+    // Parse a string date in format yyyy-mm-dd hh:mm:ss to a date object that exports
+    // correctly to Excel
+    var parseDateStrToExcel = function(d, fixmonth) {
+        if (fixmonth === undefined) fixmonth = true;
+        if (d == '') {return null;}
+
+        var datearr = d.split(' ');
+        var thisDate = datearr[0].split('-');
+        var thisTime = datearr[1].split(':');
+        var year = thisDate[0];
+        var month = thisDate[1];
+        var day = thisDate[2];
+        var hours = thisTime[0];
+        var minutes = thisTime[1];
+        var seconds = thisTime[2];
+
+        // Subtract 1 from month if month from data is not zero indexed.
+        if (fixmonth == true) {
+            month = month - 1;
+        }
+
+        var jsDate = new Date(year, month, day);
+
+        // Need to set the time parts after creating the date to get correct format
+        // in Excel
+        jsDate.setUTCHours(hours);
+        jsDate.setUTCMinutes(minutes);
+        jsDate.setUTCSeconds(seconds);
+
+        return jsDate;
+    };
+
+    // Need to add 0.71 to desired column width for Calibri 11pt font to get the width
+    // of the column in the exported file to match the desired width.
+    // https://github.com/exceljs/exceljs/issues/744
+    // Excel's default column width for Calibri 11pt font is 8.43 characters
+    var DEFAULT_COL_WIDTH = 8.43 + 0.71;
+
+    // Highcharts default csv export date format as an Excel date format
+    var DEFAULT_DATE_FORMAT = 'yyyy-mm-dd hh:mm:ss';
+
+
     H.Chart.prototype.downloadXLSX = function() {
-        // Need to add 0.71 to desired column width for Calibri 11pt font to get the width
-        // of the column in the exported file to match the desired width.
-        // https://github.com/exceljs/exceljs/issues/744
-        // Excel's default column width for Calibri 11pt font is 8.43 characters
-        var DEFAULT_COL_WIDTH = 8.43 + 0.71;
-
-        // Highcharts default csv export date format as an Excel date format
-        var DEFAULT_DATE_FORMAT = 'yyyy-mm-dd hh:mm:ss';
-
         var chart = this;
+
+        var columnHeaderFormatter = chart.options.exporting.csv.columnHeaderFormatter;
+
+        // Array of columnHeaders from getDataRows function
+        var columnHeaders = chart.getDataRows()[0];
+
+        // Boolean for datetime/non-datetime x-axis
+        var xAxisIsDatetime = chart.xAxis[0].options.type == "datetime" ? true : false;
 
         // Array of chart data rows with header row removed
         var dataRows = chart.getDataRows().slice(1);
@@ -107,84 +169,135 @@
 
         // Set export worksheet name to options.exporting.xlsx.worksheet.name or a
         // default of 'Sheet1'. Excel worksheet name length cannot exceed 31 characters
-        var worksheetName = pick(xlsxOptions.worksheet.sheetName.substring(0, 31), 'Sheet1');
-
-        // Get category column Excel number format
-        var exportCategoryFormat;
-
-        if (chart.axes[0].isDatetimeAxis) {
-            exportCategoryFormat = xlsxOptions.worksheet.categoryColumn.numberFormat || DEFAULT_DATE_FORMAT;
-        } else {
-            exportCategoryFormat = xlsxOptions.worksheet.categoryColumn.numberFormat || null;
-        }
+        var worksheetName = xlsxOptions.worksheet.sheetName ? xlsxOptions.worksheet.sheetName.substring(0, 31) : 'Sheet1';
 
         // Initialize an empty workbook and worksheet
         var workbook = new ExcelJS.Workbook();
         var worksheet = workbook.addWorksheet(worksheetName);
 
-        // Array with column header titles as they will appear when exported
-        var columnHeaders = [];
+
+        /**
+         * Get and set series column options
+         */
+
         // Array that will contain exceljs column objects
         var worksheetColumns = [];
 
-        // Add chart category column to worksheet columns array
-        var categoryColumnOptions = {}
+        chart.series.forEach(function(series, index) {
+            var seriesOptions = series.options;
+            var seriesColumnOptions = {};
 
-        // Use user-specified column header or default category column name
-        var categoryColumnHeader = xlsxOptions.worksheet.categoryColumn.title || chart.getDataRows()[0][0];
-        columnHeaders.push(categoryColumnHeader);
+            if (series.options.includeInDataExport !== false &&
+                    !series.options.isInternal &&
+                    series.visible !== false // #55
+            ) {
+
+                // Use user-specified xlsx column header if specified, if not defined,
+                // use the result of seriesColumnHeaderFormatter if defined, and finally
+                // use default series name
+                var seriesColumnHeader = seriesOptions.xlsx.name ? seriesOptions.xlsx.name : columnHeaderFormatter ? columnHeaderFormatter(series) : seriesOptions.name;
+
+                seriesColumnOptions.header = seriesColumnHeader;
+                seriesColumnOptions.key = seriesColumnHeader;
+
+                // Add number format if specified in series options
+                // var columnNumFmt = seriesOptions.xlsx.numberFormat || null;
+                if (seriesOptions.xlsx.numberFormat) {
+                    seriesColumnOptions.style = { numFmt: seriesOptions.xlsx.numberFormat };
+                    // Have to set a column width if column style is applied or the column will
+                    // not appear in the exported file.
+                    // https://github.com/exceljs/exceljs/issues/458
+                    seriesColumnOptions.width = DEFAULT_COL_WIDTH;
+                    // This width is overridden if autoFitColumns is true
+                }
+                worksheetColumns.push(seriesColumnOptions);
+            }
+        });
+
+
+        /**
+         * Get and set category column options
+         */
+
+        // Get category column header
+        var categoryColumnHeader = columnHeaders[0];
+
+        // Store index values for category columns. This is normally just the zeroth
+        // column but can be more in combination charts. We use these indices when inserting
+        // category columns into the ExcelJS worksheet columns object
+        var categoryColumnHeaderIndexes = [];
+        for(var i = 0; i < columnHeaders.length; i++) {
+            if (columnHeaders[i] === categoryColumnHeader) {
+                categoryColumnHeaderIndexes.push(i);
+            }
+        }
+
+        // Category column header definition logic is as follows:
+        // if defined: chart.options.exporting.xlsx.worksheet.categoryColumn.title
+        // else if columnHeaderFormatter defined: the result of columnHeaderFormatter for the xAxis
+        // finally: default Highcharts categoryColumnHeader
+        if (xlsxOptions.worksheet.categoryColumn.title) {
+            categoryColumnHeader = xlsxOptions.worksheet.categoryColumn.title;
+        } else if (columnHeaderFormatter) {
+            categoryColumnHeader = columnHeaderFormatter(chart.xAxis[0]);
+        }
+
+        // Category column Excel number format
+        var categoryColumnNumberFormat;
+
+        if (xAxisIsDatetime) {
+            categoryColumnNumberFormat = xlsxOptions.worksheet.categoryColumn.numberFormat || DEFAULT_DATE_FORMAT;
+        } else {
+            categoryColumnNumberFormat = xlsxOptions.worksheet.categoryColumn.numberFormat || null;
+        }
+
+        // Define category column options object and add category columns to worksheet
+        // columns array
+        var categoryColumnOptions = {};
+
+        // Set category column options that will apply to all category columns in the
+        // exported workbook.
         categoryColumnOptions.header = categoryColumnHeader;
         categoryColumnOptions.key = categoryColumnHeader;
 
-        if (exportCategoryFormat) {
-            categoryColumnOptions.style = { numFmt: exportCategoryFormat };
+        if (categoryColumnNumberFormat) {
+            categoryColumnOptions.style = { numFmt: categoryColumnNumberFormat };
             // Have to set a column width if column style is applied or the column will
             // not appear in the exported file.
             // https://github.com/exceljs/exceljs/issues/458
             categoryColumnOptions.width = DEFAULT_COL_WIDTH;
             // This width is overridden if autoFitColumns is true
         }
-        worksheetColumns.push(categoryColumnOptions);
 
-        // Add each chart series to the worksheet columns array
-        chart.series.forEach(function(series, index) {
-            var seriesOptions = series.options;
-            var seriesColumnOptions = {};
-
-            // Use user-specified column header or default series name
-            var seriesColumnHeader = seriesOptions.xlsx.name || seriesOptions.name;
-            columnHeaders.push(seriesColumnHeader);
-
-            seriesColumnOptions.header = seriesColumnHeader;
-            seriesColumnOptions.key = seriesColumnHeader;
-            // var columnOptions = { header: seriesColumnHeader, key: seriesColumnHeader }
-
-            // Add number format if specified in series options
-            // var columnNumFmt = seriesOptions.xlsx.numberFormat || null;
-            if (seriesOptions.xlsx.numberFormat) {
-                seriesColumnOptions.style = { numFmt: seriesOptions.xlsx.numberFormat };
-                // Have to set a column width if column style is applied or the column will
-                // not appear in the exported file.
-                // https://github.com/exceljs/exceljs/issues/458
-                seriesColumnOptions.width = DEFAULT_COL_WIDTH;
-                // This width is overridden if autoFitColumns is true
-            }
-            worksheetColumns.push(seriesColumnOptions);
-        });
+        // Insert the category column definitions into the appropriate indexes in the
+        // worksheetColumns array
+        for(i = 0; i < categoryColumnHeaderIndexes.length; i++) {
+            worksheetColumns.splice(categoryColumnHeaderIndexes[i], 0, categoryColumnOptions);
+        }
 
         // Add all columns to the worksheet object
         worksheet.columns = worksheetColumns;
 
         // Set date values in category column for correct Excel export if chart has
         // a datetime axis
-        if (chart.axes[0].isDatetimeAxis) {
+        if (xAxisIsDatetime) {
             dataRows.forEach(function(values, index) {
-                // Need to add 'Z' to make sure values are in UTC time
-                var jsDate = new Date(values[0] + 'Z');
-                dataRows[index][0] = jsDate;
+                dataRows[index][0] = parseDateStrToExcel(values[0]);
             });
         }
 
+        // For non category/date column columns, write #N/A formula for NaN number values
+        dataRows.forEach(function (values, index) {
+            for (var k = 1; k < values.length-1; k++) {
+                var val = dataRows[index][k];
+                if (typeof val === 'number') {
+                    if (isNaN(val)) {
+                        val = { error: '#N/A' };
+                        dataRows[index][k] = val;
+                    }
+                }
+            }
+        });
         // Add the data to the worksheet
         worksheet.addRows(dataRows);
 
@@ -192,6 +305,8 @@
         // cell with the most characters. This requires the SSF module to be loaded.
         // https://github.com/SheetJS/ssf/blob/master/ssf.js
         // https://cdn.statically.io/gh/SheetJS/ssf/e267d1d6/ssf.js
+        var wrapColumnWidth = xlsxOptions.worksheet.wrapColumnWidth || 20;
+
         if (xlsxOptions.worksheet.autoFitColumns === true) {
             // Get the length of the longest value in each column in Excel column width
             // units and set the column width.
@@ -223,6 +338,12 @@
                 // This is purely a style decision. Remove this check if you don't care
                 // about having potentially very thin columns.
                 var columnWidth = char2width(longest.length) > DEFAULT_COL_WIDTH ? char2width(longest.length) : DEFAULT_COL_WIDTH;
+                
+                if (col == 0 && columnWidth > wrapColumnWidth) {
+                    worksheet.getCell('A1').alignment = { wrapText: true };
+                    columnWidth = wrapColumnWidth;
+                }
+
                 column.width = columnWidth;
             }
         }
